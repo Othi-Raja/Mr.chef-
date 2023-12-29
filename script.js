491203968c8f22ec178da13c6320400408372621
OthiRaja <121594447+Othi-Raja@users.noreply.github.com>
2023-12-29 16:26:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ document.querySelector('#button').addEventListener('click', function () {
             console.log(result[0])
             result.forEach(recipe => {
                 const recipeItem = document.createElement('div');
-                recipeItem.innerHTML = `<div class='container' style='text-align: justify;  padding: 20px;  border: 1px solid orange; border-radius: 10px ; margin-bottom:10px; '>
-<h3><strong data-aos="fade-in">Recipe:</strong>${recipe.title}</h3>
-<p data-aos="fade-up" data-aos-delay="400" class="text-justify"><strong  data-aos="fade-up" data-aos-delay="300">Ingredients:</strong> ${recipe.ingredients}</p>
-<p data-aos="fade-up" data-aos-delay="500" class="text-justify"><strong  data-aos="fade-up" data-aos-delay="300">Servings:</strong> ${recipe.servings}</p>
-<p data-aos="fade-up" data-aos-delay="600" class="text-justify"><strong  data-aos="fade-up" data-aos-delay="300">Instructions:</strong> ${recipe.instructions}</p></div>`;
+                recipeItem.innerHTML = `<div data-aos-offset="200" class='container' style='text-align: justify;  padding: 20px;  border: 1px solid orange; border-radius: 10px ; margin-bottom:10px; '>
+<h3><strong data-aos="fade-in" data-aos-offset="200">Recipe:</strong>${recipe.title}</h3>
+<p data-aos="fade-up" data-aos-delay="400" data-aos-offset="200" class="text-justify"><strong  data-aos="fade-up" data-aos-delay="300">Ingredients:</strong> ${recipe.ingredients}</p>
+<p data-aos="fade-up" data-aos-delay="500" data-aos-offset="200" class="text-justify"><strong  data-aos="fade-up" data-aos-delay="300">Servings:</strong> ${recipe.servings}</p>
+<p data-aos="fade-up" data-aos-delay="600" data-aos-offset="200" class="text-justify"><strong  data-aos="fade-up" data-aos-delay="300">Instructions:</strong> ${recipe.instructions}</p></div>`;
                 // Append each recipe item to the recipeList or any other container
                 recipeList.appendChild(recipeItem);
             });
