69140b0ca8c5e72679533eae5989dfefa0a1e08b
OthiRaja <othiraja5677@gmail.com>
2023-12-30 09:00:03 +0530

    color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector('#button').addEventListener('click', function () {
     var query = document.querySelector('#recipe').value;
     let recipeList = document.querySelector('#recipeList');
+    const bodyc = document.querySelector('body')
     if(query== ''){
         // alert('I Dont want to be a Empty Box ')
         // document.querySelector('#alertText').innerHTML='I Dont want to be a Empty Box '
@@ -23,11 +24,12 @@ document.querySelector('#button').addEventListener('click', function () {
        
 
             document.querySelector('#search-recipe').style.height = '200px';
+            bodyc.style.backgroundColor = 'white'
             //console.log(result[0])
             result.forEach(recipe => {
                 const recipeItem = document.createElement('div');
-                recipeItem.innerHTML = `<div data-aos="fade-in"  data-aos-offset="-280" id="animation" class='container recipelist' style='padding: 20px;  border: 0.6px solid orange; border-radius: 10px ; margin-bottom:10px; '>
-<h3 data-aos="fade-up" data-aos-offset="-280"  ><strong>Recipe:</strong>${recipe.title}</h3>
+                recipeItem.innerHTML = `<div data-aos="fade-in"  data-aos-offset="-280" id="animation" class='container recipelist' style=' background-color:#F5F5F5; box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);  padding: 20px;  border: .2px solid gray;   border-width: thin; border-radius: 10px ; margin-bottom:25px; '>
+<h3 data-aos="fade-up" data-aos-offset="-280"  ><strong>Recipe:</strong>${recipe.title}</h3> <hr>
 <p data-aos="fade-up"  data-aos-offset="-280"  style='text-align: justify;'><strong>Ingredients:</strong> ${recipe.ingredients}</p>
 <p data-aos="fade-up" data-aos-offset="-280"   ><strong>Servings:</strong> ${recipe.servings}</p>
 <p data-aos="fade-up" data-aos-offset="-280"   style='text-align: justify;'><strong>Instructions:</strong> ${recipe.instructions}</p></div>`;
