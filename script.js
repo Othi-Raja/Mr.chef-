ff775ae8839f5cefbee60c0a6875c4af0e6fc5fe
Your Name <you@example.com>
2024-05-27 16:51:21 +0530

    placeholder an d aos offset

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.querySelector('#button').addEventListener('click', function () {
         document.querySelector('#recipe').placeholder='I Dont want to be an Empty Box';
        },1)
        setTimeout(function(){
-        document.querySelector('#recipe').placeholder='Type here...';
+        document.querySelector('#recipe').placeholder='Find Your Favorite Dish 😋';
        },3000)
         return false;
        }
@@ -65,11 +65,11 @@ document.querySelector('#button').addEventListener('click', function () {
             console.log(result[0])
             result.forEach(recipe => {
                 const recipeItem = document.createElement('div');
-                recipeItem.innerHTML = `<div data-aos="fade-in"  data-aos-offset="-280" id="animation" class='container recipelist' style=' background-color:#F5F5F5; box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);  padding: 20px;  border: .2px solid gray;   border-width: thin; border-radius: 10px ; margin-bottom:25px; '>
-<h3 data-aos="fade-up" data-aos-offset="-280"  ><strong>Recipe:</strong>${recipe.title}</h3> <hr>
-<p data-aos="fade-up"  data-aos-offset="-280"  style='text-align: justify;'><strong>Ingredients:</strong> ${recipe.ingredients}</p>
-<p data-aos="fade-up" data-aos-offset="-280"   ><strong>Servings:</strong> ${recipe.servings}</p>
-<p data-aos="fade-up" data-aos-offset="-280"   style='text-align: justify;'><strong>Instructions:</strong> ${recipe.instructions}</p></div>`;
+                recipeItem.innerHTML = `<div data-aos="fade-in"  data-aos-offset="-480" id="animation" class='container recipelist' style=' background-color:#F5F5F5; box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);  padding: 20px;  border: .2px solid gray;   border-width: thin; border-radius: 10px ; margin-bottom:25px; '>
+<h3 data-aos="fade-up" data-aos-offset="-480"  ><strong>Recipe:</strong>${recipe.title}</h3> <hr>
+<p data-aos="fade-up"  data-aos-offset="-480"  style='text-align: justify;'><strong>Ingredients:</strong> ${recipe.ingredients}</p>
+<p data-aos="fade-up" data-aos-offset="-480"   ><strong>Servings:</strong> ${recipe.servings}</p>
+<p data-aos="fade-up" data-aos-offset="-480"   style='text-align: justify;'><strong>Instructions:</strong> ${recipe.instructions}</p></div>`;
                 // Append each recipe item to the recipeList or any other container
                 recipeList.appendChild(recipeItem);
             });
